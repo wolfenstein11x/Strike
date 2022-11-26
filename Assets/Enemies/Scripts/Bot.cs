b9f9a7e4101f47b3a31383219aca625cbc87e17d
Charles Wolfe <cwolfe7@asu.edu>
2022-11-25 17:51:05 -0800

    robot does muzzle flash and gun sound now

--- Assets/Enemies/Scripts/Bot.cs
+++ Assets/Enemies/Scripts/Bot.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class Bot : Soldier
 {
     [SerializeField] AudioSource walkSound;
+    [SerializeField] ParticleSystem[] muzzleFlashes;
+    [SerializeField] Transform[] projectileSpawnPoints;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // here for debugging, delete later
+        FaceTarget();
     }
 
     public void PlayWalkSound()
     {
         walkSound.Play();
+    }
+
+    public void FireGun(int gunIndex)
+    {
+        gunSound.Play();
+        muzzleFlashes[gunIndex].Play();
+        muzzleFlashes[gunIndex + 1].Play();
     }
 }
