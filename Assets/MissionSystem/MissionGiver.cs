58751a6221ab9d15cb0a699d0f6d12c7498790a4
Charles Wolfe <cwolfe7@asu.edu>
2022-12-18 13:18:13 -0800

    mission status UI updates as you complete missions

--- Assets/MissionSystem/MissionGiver.cs
+++ Assets/MissionSystem/MissionGiver.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MissionGiver : MonoBehaviour
 {
     public Mission mission;
     
     PlayerStatus player;
+    MissionText missionText;
 
     [SerializeField] GameObject[] missionItems;
 
@@ -14,13 +16,31 @@ public class MissionGiver : MonoBehaviour
     {
         activateMissionItems(false);
         player = FindObjectOfType<PlayerStatus>();
+        missionText = FindObjectOfType<MissionText>();
     }
 
     public void GiveMission()
     {
+        StartCoroutine(GiveMissionCoroutine());
+        /*
         player.mission = mission;
         mission.isActive = true;
 
+        Debug.Log("New mission: " + mission.title);
+        Debug.Log(mission.description);
+
+        activateMissionItems(true);
+        */
+    }
+
+    IEnumerator GiveMissionCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        player.mission = mission;
+        mission.isActive = true;
+
+        SetMissionText();
         Debug.Log("New mission: " + mission.title);
         Debug.Log(mission.description);
 
@@ -33,5 +53,11 @@ public class MissionGiver : MonoBehaviour
         {
             item.SetActive(status);
         }
+    }
+
+    void SetMissionText()
+    {
+        TextMeshProUGUI currentMissionText = missionText.GetComponent<TextMeshProUGUI>();
+        currentMissionText.text = "Objective: " + mission.description;
     }
 }
